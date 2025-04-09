bed3d53c4fe172f10112fcc6497d23ff2890602d
horaciog1 <horaciog@nmsu.edu>
2025-04-09 17:07:18 -0600

    Alert box fixe

--- website/notification.js
+++ website/notification.js
@@ -1,38 +1,27 @@
 function invokeAlert() {
-    const scrollY = window.scrollY;       // Save scroll position
+    const scrollY = window.scrollY;
     let alertSound = new Audio("alert.mp3");
-    alertSound.volume = 0.30; // Set volume to 30%
+    alertSound.volume = 0.30;
+    alertSound.play();
 
-    // retrieve the elements needed throughout the function
-    let page = document.getElementById("page");
+    let frame = document.getElementById("screen-frame");
     let messageArea = document.getElementById("message-area");
     let alertText = document.getElementById("message");
 
-    // sound alert 
-    alertSound.play();
-
-    // higlight site border
-    page.style.border = "5px solid red";
-
-    // make message area visible
-    messageArea.style.backgroundColor = "red";
-    messageArea.style.borderRadius = "10px";
-    messageArea.style.height = "20px";
-    messageArea.style.width = "200px";
-
+    frame.style.display = "block";
+    messageArea.style.display = "flex";
+    alertText.innerText = "❕ Critical Message ❕";
     alertText.style.color = "whitesmoke";
-    window.scrollTo({ top: scrollY });      // Restore scroll position
+
+    window.scrollTo({ top: scrollY });
 }
 
 function clearAlert() {
-
-    // retrieve the elements needed throughout the function
-    let page = document.getElementById("page");
+    let frame = document.getElementById("screen-frame");
     let messageArea = document.getElementById("message-area");
     let alertText = document.getElementById("message");
 
-    // make visual alert elements invisible until next alert
-    page.style.border = "none";
-    messageArea.style.backgroundColor = "transparent";
+    frame.style.display = "none";
     alertText.style.color = "transparent";
+    messageArea.style.display = "none";
 }
