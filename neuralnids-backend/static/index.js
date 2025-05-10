693a1f8ca0d3888b18c47290a45fb7883ec304f4
Jacob Yanez <jake.yanez589@gmail.com>
2025-05-09 23:00:38 -0600

    removed extraneous comments

--- neuralnids-backend/static/index.js
+++ neuralnids-backend/static/index.js
@@ -86,14 +86,6 @@ function toggleDarkMode() {
 // Expose globally
 window.toggleDarkMode = toggleDarkMode;
 
-
-// function formatTimestamp(ts) {
-//     const d = new Date(ts);
-//     const date = d.toLocaleDateString("en-US");
-//     const time = d.toLocaleTimeString("en-US", { hour12: false });
-//     return `${date} ${time}`;
-// }
-
 async function fetchDashboardData() {
     let critAlertDetected = false;
     const scrollY = window.scrollY;       // Save scroll position
@@ -108,32 +100,10 @@ async function fetchDashboardData() {
 
     const recentAlerts = alerts.slice(-8).reverse();
 
-    // recentAlerts.forEach(alert => {
-    //     // alertCount++;
-    //     // if (alert.severity <= 2) {
-    //     //     critical++;
-    //     //     alertActive = true;
-    //     //     critAlertDetected = true;
-    //     // }
-    //     // else warning++;
-
-    //     // const proto = alert.protocol || 'Unknown';
-    //     // protocolCounts[proto] = (protocolCounts[proto] || 0) + 1;
-
-    //     // const conciseSig = alert.signature?.split("[")[0]?.trim() || "-";
-    //     // const row = document.createElement("tr");
-    //     // row.innerHTML = `<td>${formatTimestamp(alert.timestamp)}</td><td>${conciseSig}</td><td>${alert.severity}</td><td>1</td>`;
-    //     // table.appendChild(row);
-    // });
-
     if (critAlertDetected) {
         invokeAlert();
         sendEmailAlert();
     }
-
-    // document.getElementById("alert-count").innerText = alerts.length;
-    // document.getElementById("critical-count").innerText = critical;
-    // document.getElementById("warning-count").innerText = warning;
 
     if (chart) chart.destroy();
     const ctx = document.getElementById('protocol-chart').getContext('2d');
@@ -219,11 +189,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
     protocolCounts = {};
 
-    //fetchDashboardData();
     loadMap();
     fetchMLAlerts();
 
-    //setInterval(fetchDashboardData, 5000);
     setInterval(loadMap, 5000);
     setInterval(fetchMLAlerts, 5000);
 
@@ -297,9 +265,6 @@ socket.on('alert_batch', (batch) => {
                 }
             }
         });
-
-
-
         console.log(batch);
     }
 });
